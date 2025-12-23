777a695fbcd8b06b201971b7639b824f81084ed5
Rohan1o1 <143770213+Rohan1o1@users.noreply.github.com>
2025-12-24 03:00:55 +0530

    first commit

---
+++ backend/src/server.js
@@ -0,0 +1,15 @@
+import express from 'express';
+import {ENV} from './lib/env.js';
+
+
+
+const app = express();
+
+ console.log(ENV.PORT);
+ console.log(ENV.DB_URL);
+
+app.get ("/", (req, res) => {
+    res.status(200).json({ message: "Success from back2" })
+})
+
+ app.listen(ENV.PORT,()=> console.log("Server running on port 3000"));
