bee2bcc3bcb5e5b5e01118942da3557cb6cdbb14
Rohan1o1 <143770213+Rohan1o1@users.noreply.github.com>
2025-12-30 04:01:05 +0530

    corrected typos

--- backend/src/controllers/chatController.js
+++ backend/src/controllers/chatController.js
@@ -4,7 +4,7 @@ export async function getStreamToken(req, res){
         const token = chatClient.createToken(req.user.clerkId);
         res.status(200).json({
             token,
-            uderId: req.user.clerkId,
+            userId: req.user.clerkId,
             userName: req.user.name,
             userImage: req.user.image
         });
--- backend/src/middleware/protectRoute.js
+++ backend/src/middleware/protectRoute.js
@@ -1,12 +1,12 @@
 import {requireAuth} from '@clerk/express'
- import User from '../models/User.js'
+import User from '../models/User.js'
 
   export const protectRoute = [
      requireAuth(),
      async (req, res, next) => {
         try{
             const clerkId = req.auth.userId;
-             if(!clerkId)return res.status(401).json({message:"Unauthorized- inavalid token"})
+             if(!clerkId)return res.status(401).json({message:"Unauthorized- invalid token"})
             // find user in  db by clerk id 
              const user = await User.findOne({clerkId})
             if (!user) return res.status(404).json({message:"User not found"})
