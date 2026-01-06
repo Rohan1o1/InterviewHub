cf3ad82074ecfa83f3828935d04ad2adec5d8418
Rohan1o1 <143770213+Rohan1o1@users.noreply.github.com>
2026-01-07 02:50:39 +0530

    fix issue

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -4,10 +4,7 @@ import './index.css'
 import App from './App.jsx'
 import { ClerkProvider } from '@clerk/clerk-react'
 import { BrowserRouter } from 'react-router'
-import{
-  QueryClient,
-  QueryClientProvider,} 
-  from '@tanstack/react-query'
+import{QueryClient,QueryClientProvider} from '@tanstack/react-query'
 
 
 const PUBLISHABLE_KEY = import.meta.env.VITE_CLERK_PUBLISHABLE_KEY
